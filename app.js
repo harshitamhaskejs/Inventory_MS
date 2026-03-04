7c8eeab731d2ac005936671a458e51979a3557ab
Rudra Joshi <rudra.joshi@jerseystem.org>
2026-03-03 20:11:03 -0500

    Add complete Firebase config including storageBucket and measurementId

--- app.js
+++ app.js
@@ -31,8 +31,10 @@ async function registerPush() {
       apiKey: "AIzaSyAVEEyXIgAgseGFEPt4i6nezAS_6TXXuKs",
       authDomain: "imsapp-619b6.firebaseapp.com",
       projectId: "imsapp-619b6",
+      storageBucket: "imsapp-619b6.firebasestorage.app",
       messagingSenderId: "547704030976",
       appId: "1:547704030976:web:d5c5ad3545b213fd77d5e5",
+      measurementId: "G-JZ1HQX5NET"
     });
 
     const messaging = messagingLib.getMessaging(app);
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,8 +5,10 @@ firebase.initializeApp({
   apiKey: "AIzaSyAVEEyXIgAgseGFEPt4i6nezAS_6TXXuKs",
   authDomain: "imsapp-619b6.firebaseapp.com",
   projectId: "imsapp-619b6",
+  storageBucket: "imsapp-619b6.firebasestorage.app",
   messagingSenderId: "547704030976",
   appId: "1:547704030976:web:d5c5ad3545b213fd77d5e5",
+  measurementId: "G-JZ1HQX5NET"
 });
 
 const messaging = firebase.messaging();
